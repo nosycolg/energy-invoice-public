9c24edab81458bbf4c101cfe64163d24c55f7eb1
Cristhian Felipe <cristhianfelipeyt@gmail.com>
2026-03-01 23:08:46 -0300

    feat: migrate configuration from next.config.ts to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // Permite chamadas server-side para a API do backend
 };
 
